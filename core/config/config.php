ed558706154f4ed429694cf838f93e8b4f0a8ce9
Mauricio Gutierrez <developer.mauricio2310@gmail.com>
2020-05-07 16:15:26 +0000

    first commit

---
+++ core/config/config.php
@@ -0,0 +1,10 @@
+<?php
+defined('ALTUMCODE') || die();
+
+$config = [
+    'database_host'        => '127.0.0.1',
+    'database_username'    => 'forge',
+    'database_password'    => '8xaGFxCBCZlU0rYNnhNI',
+    'database_name'        => 'social',
+    'url'                  => 'https://social.llorenteads.com/'
+];
