0a846aa87d28e494bc51023dab9338535e2959a7
Omar David Sanchez Arroyo <odsanchez46@misena.edu.co>
2020-05-20 06:26:01 -0500

    Añadido usuario db

--- core/config/config.php
+++ core/config/config.php
@@ -3,8 +3,8 @@ defined('ALTUMCODE') || die();
 
 $config = [
     'database_host'        => '127.0.0.1',
-    'database_username'    => 'todos',
-    'database_password'    => '123456',
+    'database_username'    => 'forge',
+    'database_password'    => '8xaGFxCBCZlU0rYNnhNI',
     'database_name'        => 'social',
-    'url'                  => 'http://social.test/'
+    'url'                  => 'https://social.llorenteads.com/'
 ];
