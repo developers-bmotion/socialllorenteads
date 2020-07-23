c1741a00c576f6830c5b480db1d5b7e0a03d5d36
Omar David Sanchez Arroyo <odsanchez46@misena.edu.co>
2020-07-23 11:32:07 -0500

    Añadida configuración adicional

--- core/config/config.php
+++ core/config/config.php
@@ -4,7 +4,7 @@ defined('ALTUMCODE') || die();
 $config = [
     'database_host'        => '127.0.0.1',
     'database_username'    => 'forge',
-    'database_password'    => '8xaGFxCBCZlU0rYNnhNI',
+    'database_password'    => 'SRnJ04NcPbna2SlJsciu',
     'database_name'        => 'social',
     'url'                  => 'https://social.llorenteads.com/',
     'key_scraper'          => 'a23667ae8dfbbbc2b8a9fc21cc645fae'
